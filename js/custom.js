5b73cb59d21746303fc9080e73d95747f545f5fd
Guru707 <gsraj77@gmail.com>
2020-09-14 19:33:29 +0530

    content(homepage)change content homepage

--- js/custom.js
+++ js/custom.js
@@ -198,3 +198,45 @@ function resetWheel()
 //     console.log
 //   }
 // });
+
+$(document).ready(function () {
+  $('#slider-items').slick({
+     infinite: true,
+     dots: true,
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     vertical: true,
+     arrows: false,
+     verticalSwiping: true,
+     aitForAnimate: true,
+     autoplay: true,
+     responsive: [
+{
+  breakpoint: 767,
+  settings: {
+    dots: false,
+  }
+}
+]
+  });
+  $("#toggle-btn").click(function(){
+     $(".side-menu").addClass("active");
+     $(".side-menu").addClass("active");
+     
+
+  });
+  $(".cross").click(function(){        
+     $(".side-menu").removeClass("active"); 
+  });
+});
+
+$(function() {
+  $('.popup-youtube').magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+      fixedContentPos: false
+  });
+});
